8e2ddd9beaed81fa0b087bb035ae87945d14bacc
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2024-02-26 21:24:57 +0900

    Change Folder, Add Derived class

---
+++ Aura/Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,12 @@
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Aura/Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Default
+
+
+#include "Character/AuraEnemy.h"
+
