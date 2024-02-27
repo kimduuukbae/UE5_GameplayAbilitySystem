6c571d0ebb6aa7e94a95bf8269a8180ba7cd33bd
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2024-02-27 16:13:41 +0900

    Impl GameMode, Player SpringArm,Camera

--- Aura/Source/Aura/Private/Character/AuraPlayerCharacter.cpp
+++ Aura/Source/Aura/Private/Character/AuraPlayerCharacter.cpp
@@ -1,2 +1,36 @@
 #include "Character/AuraPlayerCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringarmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+AAuraPlayerCharacter::AAuraPlayerCharacter()
+{
+	// SpringArm and Camera do not need to be written in C++.
+	// There are no performance benefits compared to blueprints.
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 750.0f;
+	SpringArm->SetWorldRotation(FRotator(-45.0f, 0.0f, 0.0f));
+	SpringArm->bUsePawnControlRotation = false;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->bInheritYaw = false;
+	SpringArm->bEnableCameraLag = true;
+	
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
+
+	UCharacterMovementComponent* MovementComponent = GetCharacterMovement();
+	
+	MovementComponent->bOrientRotationToMovement = true;
+	MovementComponent->RotationRate = FRotator(0.0f, 400.0f, 0.0f);
+	MovementComponent->bConstrainToPlane = true;
+	MovementComponent->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
--- Aura/Source/Aura/Public/Character/AuraPlayerCharacter.h
+++ Aura/Source/Aura/Public/Character/AuraPlayerCharacter.h
@@ -4,9 +4,20 @@
 #include "Character/AuraCharacterBase.h"
 #include "AuraPlayerCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class AURA_API AAuraPlayerCharacter : public AAuraCharacterBase
 {
 	GENERATED_BODY()
+public:
+	AAuraPlayerCharacter();
 	
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	TObjectPtr<UCameraComponent> Camera;
 };
