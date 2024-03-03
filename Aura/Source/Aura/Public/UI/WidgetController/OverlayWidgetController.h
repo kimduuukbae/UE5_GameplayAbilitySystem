79c91612f5050b5f8cf230b75c5bccc1c144402a
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2024-03-03 14:03:42 +0900

    add delegate, bind

--- Aura/Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Aura/Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -4,9 +4,20 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "OverlayWidgetController.generated.h"
 
-UCLASS()
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangeDelegate, float, NewHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangeDelegate, float, NewMaxHealth);
+
+UCLASS(BlueprintType, Blueprintable)
 class AURA_API UOverlayWidgetController : public UAuraWidgetController
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void BroadcastInitialValues() override;
+
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FOnHealthChangeDelegate OnHealthChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FOnMaxHealthChangeDelegate OnMaxHealthChanged;
 };
