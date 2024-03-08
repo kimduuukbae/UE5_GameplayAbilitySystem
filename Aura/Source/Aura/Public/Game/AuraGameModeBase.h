08cfceffc849a1667fd799f75eec3d7a7efd584e
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2024-03-08 18:49:58 +0900

    delete comment

--- Aura/Source/Aura/Public/Game/AuraGameModeBase.h
+++ Aura/Source/Aura/Public/Game/AuraGameModeBase.h
@@ -1,14 +1,9 @@
-// Default
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "AuraGameModeBase.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class AURA_API AAuraGameModeBase : public AGameModeBase
 {
